28cde6eb7f36276a2292b7c72fda00bc772d8740
Nkemjika1divine <cndivine@gmail.com>
2023-04-25 12:42:21 -0700

    print reverse

---
+++ print_rev.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_reverse - function that prints reverse string.
+ *
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ *
+ * Return: Numbers of chars printed
+ */
+
+int print_reverse(va_list types, char buffer[], int flags,
+	int width, int precision, int size)
+{
+	char *str;
+	int a, count = 0;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(size);
+
+	str = va_arg(types, char *);
+
+	if (str == NULL)
+	{
+		UNUSED(precision);
+
+		str = ")Null(";
+	}
+	for (a = 0; str[a]; a++)
+		;
+
+	for (a = a - 1; a >= 0; a--)
+	{
+		char z = str[a];
+
+		write(1, &z, 1);
+		count++;
+	}
+	return (count);
+}
