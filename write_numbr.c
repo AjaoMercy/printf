b0254df0758d5a3200e8676968a491c668cf278b
Nkemjika1divine <cndivine@gmail.com>
2023-04-25 12:55:16 -0700

    write number

---
+++ write_numbr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * write_number - function that prints a string
+ *
+ * @is_negative: Lista of arguments
+ * @ind: char types.
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: precision specifier
+ * @size: Size specifier
+ *
+ * Return: Number of chars printed.
+ */
+
+int write_number(int is_negative, int ind, char buffer[], int flags,
+	int width, int precision, int size)
+{
+	/*variables*/
+	/* initializes length to the length of the number string in the buffer.*/
+	int length = BUFFER_SIZE - ind - 1;
+	char padd = ' ', extra_ch = 0;
+
+	UNUSED(size);
+
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padd = '0';
+	if (is_negative)
+		extra_ch = '-';
+	else if (flags & F_PLUS)
+		extra_ch = '+';
+	else if (flags & F_SPACE)
+		extra_ch = ' ';
+
+	return (write_num(ind, buffer, flags, width, precision,
+		length, padd, extra_ch));
+}
